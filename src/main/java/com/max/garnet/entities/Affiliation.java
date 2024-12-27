ce05437746340d400b5b832f71b8bd5d32a6b898
nguyenlamuyen96 <105040660+nguyenlamuyen96@users.noreply.github.com>
2024-12-28 06:53:58 +0700

    -add allifilation table

---
+++ src/main/java/com/max/garnet/entities/Affiliation.java
@@ -0,0 +1,38 @@
+package com.max.garnet.entities;
+
+import java.security.Timestamp;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "affiliations")
+public class Affiliation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Affiliation parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Affiliation> children;
+
+    @Column(updatable = false)
+    private Timestamp created_at;
+
+    private Timestamp updated_at;
+}
